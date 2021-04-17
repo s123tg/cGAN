1e7a4151e4ea4c5a42783685164d5d345e14a79d
Staler2019 <abcd121234@yahoo.com.tw>
2021-04-17 21:15:56 +0800

    midterm check v0.1.0

---
+++ create_env.bat
@@ -0,0 +1,3 @@
+pip3 install virtualenv
+python -m venv venv
+.\venv\Scripts\activate.bat
--- gan.py
+++ gan.py
@@ -1,217 +1,84 @@
-# this is for tensorflow version < 2.0
-
-"""
-This is a straightforward Python implementation of a generative adversarial network.
-The code is drawn directly from the O'Reilly interactive tutorial on GANs
-(https://www.oreilly.com/learning/generative-adversarial-networks-for-beginners).
-
-A version of this model with explanatory notes is also available on GitHub
-at https://github.com/jonbruner/generative-adversarial-networks.
-
-This script requires TensorFlow and its dependencies in order to run. Please see
-the readme for guidance on installing TensorFlow.
-
-This script won't print summary statistics in the terminal during training;
-track progress and see sample images in TensorBoard.
-"""
-
-import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
-import datetime
+import random
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import Model
+from tensorflow.keras import Sequential
+import tensorflow.keras.backend as K
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
+from os import system
+(train_x, train_y), (test_x, test_y) = mnist.load_data()
+train_x = train_x.reshape([-1, 28 * 28]) / 255  # flatten后归一化
+# 生成器
+generator = Sequential([
+    Dense(128, activation='relu', input_shape=[100]),
+    Dense(28 * 28, activation='sigmoid')
+])
 
-# Load MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/")
+# 判别器
+discriminator = Sequential([
+    Dense(128, activation='relu', input_shape=[28 * 28]),
+    Dense(1, activation='sigmoid')
+])
 
-# Define the discriminator network
+g_sample_input = Input([100])  # 生成器输入
+x_input = Input([28 * 28])  # 真实数据输入
 
+# 裁剪概率到区间[1e-6, 1]内，并求其log，避免log后为inf，K.stop_gradient表示训练时不对其求梯度
+#   这里也可直接写成 log_clip = Lambda(lambda x: K.log(x + 1e-3))
+log_clip = Lambda(lambda x: K.log(
+    K.clip(K.stop_gradient(x), 1e-6, 1) - K.stop_gradient(x) + x))
 
-def discriminator(images, reuse_variables=None):
-    with tf.variable_scope(tf.get_variable_scope(), reuse=reuse_variables) as scope:
-        # First convolutional and pool layers
-        # This finds 32 different 5 x 5 pixel features
-        d_w1 = tf.get_variable(
-            'd_w1', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
-        d_b1 = tf.get_variable(
-            'd_b1', [32], initializer=tf.constant_initializer(0))
-        d1 = tf.nn.conv2d(input=images, filter=d_w1, strides=[
-                          1, 1, 1, 1], padding='SAME')
-        d1 = d1 + d_b1
-        d1 = tf.nn.relu(d1)
-        d1 = tf.nn.avg_pool(d1, ksize=[1, 2, 2, 1], strides=[
-                            1, 2, 2, 1], padding='SAME')
+g = discriminator(generator(g_sample_input))  # 假数据
 
-        # Second convolutional and pool layers
-        # This finds 64 different 5 x 5 pixel features
-        d_w2 = tf.get_variable(
-            'd_w2', [5, 5, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
-        d_b2 = tf.get_variable(
-            'd_b2', [64], initializer=tf.constant_initializer(0))
-        d2 = tf.nn.conv2d(input=d1, filter=d_w2, strides=[
-                          1, 1, 1, 1], padding='SAME')
-        d2 = d2 + d_b2
-        d2 = tf.nn.relu(d2)
-        d2 = tf.nn.avg_pool(d2, ksize=[1, 2, 2, 1], strides=[
-                            1, 2, 2, 1], padding='SAME')
+# 判别器loss
+d_loss = (
+    - log_clip(discriminator(x_input))
+    - log_clip(1.0 - g)
+)
 
-        # First fully connected layer
-        d_w3 = tf.get_variable('d_w3', [
-                               7 * 7 * 64, 1024], initializer=tf.truncated_normal_initializer(stddev=0.02))
-        d_b3 = tf.get_variable(
-            'd_b3', [1024], initializer=tf.constant_initializer(0))
-        d3 = tf.reshape(d2, [-1, 7 * 7 * 64])
-        d3 = tf.matmul(d3, d_w3)
-        d3 = d3 + d_b3
-        d3 = tf.nn.relu(d3)
+fit_discriminator = Model(
+    inputs=[x_input, g_sample_input], outputs=d_loss)  # 训练discriminator所用模型
+fit_discriminator.add_loss(d_loss)  # 添加自定义loss
 
-        # Second fully connected layer
-        d_w4 = tf.get_variable(
-            'd_w4', [1024, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
-        d_b4 = tf.get_variable(
-            'd_b4', [1], initializer=tf.constant_initializer(0))
-        d4 = tf.matmul(d3, d_w4) + d_b4
+# 在调用compile之前置generator.trainable为False，调用compile后的模型训练时不更新generator的参数
+generator.trainable = False
+fit_discriminator.compile(optimizer=Adam(0.001))
+generator.trainable = True
 
-        # d4 contains unscaled values
-        return d4
+# 生成器loss
+g_loss = (
+    - log_clip(g)
+)
 
-# Define the generator network
+fit_generator = Model(inputs=g_sample_input, outputs=g_loss)  # 训练generator所用模型
+fit_generator.add_loss(g_loss)
 
-
-def generator(z, batch_size, z_dim):
-    g_w1 = tf.get_variable('g_w1', [z_dim, 3136], dtype=tf.float32,
-                           initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g_b1 = tf.get_variable(
-        'g_b1', [3136], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g1 = tf.matmul(z, g_w1) + g_b1
-    g1 = tf.reshape(g1, [-1, 56, 56, 1])
-    g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='g_b1')
-    g1 = tf.nn.relu(g1)
-
-    # Generate 50 features
-    g_w2 = tf.get_variable('g_w2', [3, 3, 1, z_dim/2], dtype=tf.float32,
-                           initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g_b2 = tf.get_variable(
-        'g_b2', [z_dim/2], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g2 = tf.nn.conv2d(g1, g_w2, strides=[1, 2, 2, 1], padding='SAME')
-    g2 = g2 + g_b2
-    g2 = tf.contrib.layers.batch_norm(g2, epsilon=1e-5, scope='g_b2')
-    g2 = tf.nn.relu(g2)
-    g2 = tf.image.resize_images(g2, [56, 56])
-
-    # Generate 25 features
-    g_w3 = tf.get_variable('g_w3', [3, 3, z_dim/2, z_dim/4], dtype=tf.float32,
-                           initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g_b3 = tf.get_variable(
-        'g_b3', [z_dim/4], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g3 = tf.nn.conv2d(g2, g_w3, strides=[1, 2, 2, 1], padding='SAME')
-    g3 = g3 + g_b3
-    g3 = tf.contrib.layers.batch_norm(g3, epsilon=1e-5, scope='g_b3')
-    g3 = tf.nn.relu(g3)
-    g3 = tf.image.resize_images(g3, [56, 56])
-
-    # Final convolution with one output channel
-    g_w4 = tf.get_variable('g_w4', [1, 1, z_dim/4, 1], dtype=tf.float32,
-                           initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g_b4 = tf.get_variable(
-        'g_b4', [1], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g4 = tf.nn.conv2d(g3, g_w4, strides=[1, 2, 2, 1], padding='SAME')
-    g4 = g4 + g_b4
-    g4 = tf.sigmoid(g4)
-
-    # Dimensions of g4: batch_size x 28 x 28 x 1
-    return g4
-
-
-z_dimensions = 100
-batch_size = 50
-z_placeholder = tf.placeholder(
-    tf.float32, [None, z_dimensions], name='z_placeholder')
-# z_placeholder is for feeding input noise to the generator
-
-x_placeholder = tf.placeholder(
-    tf.float32, shape=[None, 28, 28, 1], name='x_placeholder')
-# x_placeholder is for feeding input images to the discriminator
-
-Gz = generator(z_placeholder, batch_size, z_dimensions)
-# Gz holds the generated images
-
-Dx = discriminator(x_placeholder)
-# Dx will hold discriminator prediction probabilities
-# for the real MNIST images
-
-Dg = discriminator(Gz, reuse_variables=True)
-# Dg will hold discriminator prediction probabilities for generated images
-
-# Define losses
-d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-    logits=Dx, labels=tf.ones_like(Dx)))
-d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-    logits=Dg, labels=tf.zeros_like(Dg)))
-g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-    logits=Dg, labels=tf.ones_like(Dg)))
-
-# Define variable lists
-tvars = tf.trainable_variables()
-d_vars = [var for var in tvars if 'd_' in var.name]
-g_vars = [var for var in tvars if 'g_' in var.name]
-
-# Define the optimizers
-# Train the discriminator
-d_trainer_fake = tf.train.AdamOptimizer(
-    0.0003).minimize(d_loss_fake, var_list=d_vars)
-d_trainer_real = tf.train.AdamOptimizer(
-    0.0003).minimize(d_loss_real, var_list=d_vars)
-
-# Train the generator
-g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
-
-# From this point forward, reuse variables
-tf.get_variable_scope().reuse_variables()
-
-sess = tf.Session()
-
-# Send summary statistics to TensorBoard
-tf.summary.scalar('Generator_loss', g_loss)
-tf.summary.scalar('Discriminator_loss_real', d_loss_real)
-tf.summary.scalar('Discriminator_loss_fake', d_loss_fake)
-
-images_for_tensorboard = generator(z_placeholder, batch_size, z_dimensions)
-tf.summary.image('Generated_images', images_for_tensorboard, 5)
-merged = tf.summary.merge_all()
-logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
-writer = tf.summary.FileWriter(logdir, sess.graph)
-
-sess.run(tf.global_variables_initializer())
-
-# Pre-train discriminator
-for i in range(300):
-    z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
-    real_image_batch = mnist.train.next_batch(
-        batch_size)[0].reshape([batch_size, 28, 28, 1])
-    _, __, dLossReal, dLossFake = sess.run([d_trainer_real, d_trainer_fake, d_loss_real, d_loss_fake],
-                                           {x_placeholder: real_image_batch, z_placeholder: z_batch})
-
-# Train generator and discriminator together
-for i in range(100000):
-    real_image_batch = mnist.train.next_batch(
-        batch_size)[0].reshape([batch_size, 28, 28, 1])
-    z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
-
-    # Train discriminator on both real and fake images
-    _, __, dLossReal, dLossFake = sess.run([d_trainer_real, d_trainer_fake, d_loss_real, d_loss_fake],
-                                           {x_placeholder: real_image_batch, z_placeholder: z_batch})
-
-    # Train generator
-    z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
-    _ = sess.run(g_trainer, feed_dict={z_placeholder: z_batch})
-
-    if i % 10 == 0:
-        # Update TensorBoard with summary statistics
-        z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
-        summary = sess.run(
-            merged, {z_placeholder: z_batch, x_placeholder: real_image_batch})
-        writer.add_summary(summary, i)
-
-# Optionally, uncomment the following lines to update the checkpoint files attached to the tutorial.
-# saver = tf.train.Saver()
-# saver.save(sess, 'pretrained-model/pretrained_gan.ckpt')
+# 生成器训练时不更新discriminator的参数
+discriminator.trainable = False
+fit_generator.compile(optimizer=Adam(0.001))
+discriminator.trainable = True
+batch_size = 64
+for i in range(20000):
+    # if i % 2000 == 0:
+    # system("cls")
+    #plt.imshow(generator.predict(np.random.uniform(-1, 1, [1, 100]))[0].reshape([28, 28]), cmap='gray')
+    # plt.show()
+    # print(i)
+    # 随机选取batch_size个真样本
+    x = train_x[random.sample(range(len(train_x)), batch_size)]
+    # 生成batch_size个随机数据输入
+    g_sample = np.random.uniform(-1, 1, [batch_size, 100])
+    # 训练辨别器，多输入需传入一个包含多个tensor的列表，此处用K.constant代替
+    fit_discriminator.fit([K.constant(x), K.constant(g_sample)])
+    fit_generator.fit(g_sample)  # 训练生成器
+fig, axes = plt.subplots(10, 10, figsize=(10, 10))
+for i in range(10):
+    for j in range(10):
+        axes[i, j].imshow(generator.predict(
+            np.random.uniform(-1, 1, [1, 100]))[0].reshape([28, 28]), cmap='gray')
+        axes[i, j].axis(False)
+plt.show()
