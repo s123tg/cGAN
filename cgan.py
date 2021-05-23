4f8838006b8a91227c6519f4e81736a650fa37f7
Staler2019 <abcd121234@yahoo.com.tw>
2021-05-23 19:25:08 +0800

    change formatter to black

--- cgan.py
+++ cgan.py
@@ -20,70 +20,76 @@ train_x = train_x.reshape([-1, 28, 28, 1]) / 255  # flatten后归一化
 
 
 # 生成器 generator
-g_sequential = Sequential([
-    Dense(7 * 7 * 64, input_shape=[100 + 10]),
-    BatchNormalization(),
-    LeakyReLU(),
-    Reshape([7, 7, 64]),
-    UpSampling2D([2, 2]),
-    Conv2DTranspose(64, [3, 3], padding='same'),
-    BatchNormalization(),
-    LeakyReLU(),
-    UpSampling2D([2, 2]),
-    Conv2DTranspose(1, [3, 3], padding='same', activation='sigmoid')
-])
+g_sequential = Sequential(
+    [
+        Dense(7 * 7 * 64, input_shape=[100 + 10]),
+        BatchNormalization(),
+        LeakyReLU(),
+        Reshape([7, 7, 64]),
+        UpSampling2D([2, 2]),
+        Conv2DTranspose(64, [3, 3], padding="same"),
+        BatchNormalization(),
+        LeakyReLU(),
+        UpSampling2D([2, 2]),
+        Conv2DTranspose(1, [3, 3], padding="same", activation="sigmoid"),
+    ]
+)
 
 # 判别器 discriminator
-discriminator = Sequential([
-    Conv2D(64, [3, 3], padding='same', input_shape=[28, 28, 1]),
-    BatchNormalization(),
-    LeakyReLU(),
-    MaxPool2D([2, 2]),
-    Conv2D(64, [3, 3], padding='same'),
-    BatchNormalization(),
-    LeakyReLU(),
-    MaxPool2D([2, 2]),
-    Flatten(),
-    Dense(128),
-    BatchNormalization(),
-    LeakyReLU(),
-    Dense(11, activation='softmax')
-])
+discriminator = Sequential(
+    [
+        Conv2D(64, [3, 3], padding="same", input_shape=[28, 28, 1]),
+        BatchNormalization(),
+        LeakyReLU(),
+        MaxPool2D([2, 2]),
+        Conv2D(64, [3, 3], padding="same"),
+        BatchNormalization(),
+        LeakyReLU(),
+        MaxPool2D([2, 2]),
+        Flatten(),
+        Dense(128),
+        BatchNormalization(),
+        LeakyReLU(),
+        Dense(11, activation="softmax"),
+    ]
+)
 
 g_sample_input = Input([100])  # 生成器输入
-g_label_input = Input([], dtype='int32')  # 指定标签输入
+g_label_input = Input([], dtype="int32")  # 指定标签输入
 x_input = Input([28, 28, 1])  # 真实样本输入
-x_label_input = Input([], dtype='int32')  # 真实样本标签输入
+x_label_input = Input([], dtype="int32")  # 真实样本标签输入
 
 condition_g_sample_input = K.concatenate(
-    [g_sample_input, K.one_hot(g_label_input, 10)])  # 合并随机数据输入与指定标签独热码
+    [g_sample_input, K.one_hot(g_label_input, 10)]
+)  # 合并随机数据输入与指定标签独热码
 
 g_output = g_sequential(condition_g_sample_input)  # 生成器输出
-generator = Model(inputs=[g_sample_input, g_label_input],
-                  outputs=g_output)  # 生成器模型
+generator = Model(inputs=[g_sample_input, g_label_input], outputs=g_output)  # 生成器模型
 
 # 裁剪概率到区间[1e-3, 1]内，并求其log，避免log后为inf，K.stop_gradient表示训练时不对其求梯度
 #   这里也可直接写成 log_clip = Lambda(lambda x: K.log(x + 1e-3))
-log_clip = Lambda(lambda x: K.log(
-    K.clip(K.stop_gradient(x), 1e-3, 1) - K.stop_gradient(x) + x))
-
-g_prob = discriminator(
-    generator([g_sample_input, g_label_input]))  # 判别器识别假样本的输出
-g_index = K.stack([K.arange(0, K.shape(g_prob)[0]),
-                  g_label_input], axis=1)  # 用于索引g_prob指定标签概率值
-
-d_prob = discriminator(x_input)  # 判别器识别真实样本的输出
-x_index = K.stack([K.arange(0, K.shape(d_prob)[0]),
-                  x_label_input], axis=1)  # 用于索引d_prob正确标签概率值
-
-
-d_loss = (
-    - log_clip(tf.gather_nd(d_prob, x_index))  # log(真实样本正确标签概率值)
-    - log_clip(1.0 - tf.gather_nd(g_prob, g_index))  # log(1-假样本指定标签的概率值)
+log_clip = Lambda(
+    lambda x: K.log(K.clip(K.stop_gradient(x), 1e-3, 1) - K.stop_gradient(x) + x)
 )
 
+g_prob = discriminator(generator([g_sample_input, g_label_input]))  # 判别器识别假样本的输出
+g_index = K.stack(
+    [K.arange(0, K.shape(g_prob)[0]), g_label_input], axis=1
+)  # 用于索引g_prob指定标签概率值
+
+d_prob = discriminator(x_input)  # 判别器识别真实样本的输出
+x_index = K.stack(
+    [K.arange(0, K.shape(d_prob)[0]), x_label_input], axis=1
+)  # 用于索引d_prob正确标签概率值
+
+
+d_loss = -log_clip(tf.gather_nd(d_prob, x_index)) - log_clip(  # log(真实样本正确标签概率值)
+    1.0 - tf.gather_nd(g_prob, g_index)
+)  # log(1-假样本指定标签的概率值)
+
 fit_discriminator = Model(
-    inputs=[g_sample_input, g_label_input, x_input, x_label_input], outputs=d_loss)
+    inputs=[g_sample_input, g_label_input, x_input, x_label_input], outputs=d_loss
+)
 fit_discriminator.add_loss(d_loss)  # 添加自定义loss
 generator.trainable = False
 for layer in generator.layers:
@@ -93,9 +99,7 @@ fit_discriminator.compile(optimizer=Adam(0.001))
 generator.trainable = True
 
 
-g_loss = (
-    -log_clip(tf.gather_nd(g_prob, g_index))  # log(假样本指定标签的概率值)
-)
+g_loss = -log_clip(tf.gather_nd(g_prob, g_index))  # log(假样本指定标签的概率值)
 
 
 fit_generator = Model(inputs=[g_sample_input, g_label_input], outputs=g_loss)
@@ -126,15 +130,20 @@ for i in range(10000):
     g_sample = np.random.uniform(-1, 1, [batch_size, 100])
     g_label = np.random.randint(0, 10, [batch_size])
 
-    fit_discriminator.fit([K.constant(g_sample), K.constant(
-        g_label), K.constant(x), K.constant(x_label)])
+    fit_discriminator.fit(
+        [K.constant(g_sample), K.constant(g_label), K.constant(x), K.constant(x_label)]
+    )
     fit_generator.fit([K.constant(g_sample), K.constant(g_label)])
 
 # image show
 fig, axes = plt.subplots(10, 10, figsize=(10, 10))
 for i in range(10):
     for j in range(10):
-        axes[i, j].imshow(generator.predict([K.constant(
-            np.random.uniform(-1, 1, [1, 100])), K.constant([i])])[0].reshape([28, 28]), cmap='gray')
+        axes[i, j].imshow(
+            generator.predict(
+                [K.constant(np.random.uniform(-1, 1, [1, 100])), K.constant([i])]
+            )[0].reshape([28, 28]),
+            cmap="gray",
+        )
         axes[i, j].axis(False)
 plt.show()
--- gan.py
+++ gan.py
@@ -16,35 +16,38 @@ from os import system
 train_x = train_x.reshape([-1, 28 * 28]) / 255  # flatten后归一化
 
 # 生成器
-generator = Sequential([
-    Dense(128, activation='relu', input_shape=[100]),
-    Dense(28 * 28, activation='sigmoid')
-])
+generator = Sequential(
+    [
+        Dense(128, activation="relu", input_shape=[100]),
+        Dense(28 * 28, activation="sigmoid"),
+    ]
+)
 
 # 判别器
-discriminator = Sequential([
-    Dense(128, activation='relu', input_shape=[28 * 28]),
-    Dense(1, activation='sigmoid')
-])
+discriminator = Sequential(
+    [
+        Dense(128, activation="relu", input_shape=[28 * 28]),
+        Dense(1, activation="sigmoid"),
+    ]
+)
 
 g_sample_input = Input([100])  # 生成器输入
 x_input = Input([28 * 28])  # 真实数据输入
 
 # 裁剪概率到区间[1e-6, 1]内，并求其log，避免log后为inf，K.stop_gradient表示训练时不对其求梯度
 #   这里也可直接写成 log_clip = Lambda(lambda x: K.log(x + 1e-3))
-log_clip = Lambda(lambda x: K.log(
-    K.clip(K.stop_gradient(x), 1e-6, 1) - K.stop_gradient(x) + x))
+log_clip = Lambda(
+    lambda x: K.log(K.clip(K.stop_gradient(x), 1e-6, 1) - K.stop_gradient(x) + x)
+)
 
 g = discriminator(generator(g_sample_input))  # 假数据
 
 # 判别器loss
-d_loss = (
-    - log_clip(discriminator(x_input))
-    - log_clip(1.0 - g)
-)
+d_loss = -log_clip(discriminator(x_input)) - log_clip(1.0 - g)
 
 fit_discriminator = Model(
-    inputs=[x_input, g_sample_input], outputs=d_loss)  # 训练discriminator所用模型
+    inputs=[x_input, g_sample_input], outputs=d_loss
+)  # 训练discriminator所用模型
 fit_discriminator.add_loss(d_loss)  # 添加自定义loss
 
 # 在调用compile之前置generator.trainable为False，调用compile后的模型训练时不更新generator的参数
@@ -53,9 +56,7 @@ fit_discriminator.compile(optimizer=Adam(0.001))
 generator.trainable = True
 
 # 生成器loss
-g_loss = (
-    - log_clip(g)
-)
+g_loss = -log_clip(g)
 
 fit_generator = Model(inputs=g_sample_input, outputs=g_loss)  # 训练generator所用模型
 fit_generator.add_loss(g_loss)
@@ -71,7 +72,7 @@ batch_size = 64
 for i in range(20000):
     # if i % 2000 == 0:
     # system("cls")
-    #plt.imshow(generator.predict(np.random.uniform(-1, 1, [1, 100]))[0].reshape([28, 28]), cmap='gray')
+    # plt.imshow(generator.predict(np.random.uniform(-1, 1, [1, 100]))[0].reshape([28, 28]), cmap='gray')
     # plt.show()
     # print(i)
     # 随机选取batch_size个真样本
@@ -87,7 +88,9 @@ for i in range(20000):
 fig, axes = plt.subplots(10, 10, figsize=(10, 10))
 for i in range(10):
     for j in range(10):
-        axes[i, j].imshow(generator.predict(
-            np.random.uniform(-1, 1, [1, 100]))[0].reshape([28, 28]), cmap='gray')
+        axes[i, j].imshow(
+            generator.predict(np.random.uniform(-1, 1, [1, 100]))[0].reshape([28, 28]),
+            cmap="gray",
+        )
         axes[i, j].axis(False)
 plt.show()
